a759c48ae82afb27b75b0b08accb7f082eda7401
Sohail7000 <sohailansari1969@gmail.com>
2023-12-07 12:47:57 +0530

    first commit with responsive navbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -2,33 +2,43 @@ import { useRef } from 'react';
 import { FaBars, FaTimes } from 'react-icons/fa';
 import '../Styles/main.css';
 import logo from '../assets/bpa.png';
-import { Link } from "react-router-dom";
-
-
+import { Link } from 'react-router-dom';
 
 function Navbar() {
+    
   const navRef = useRef();
 
   const showNavbar = () => {
     navRef.current.classList.toggle('responsive_nav');
   };
 
+  const closeNavbar = () => {
+    navRef.current.classList.remove('responsive_nav');
+  };
+
   return (
     <header>
-      {/* <h3>LOGO</h3> */}
-      <img className="logo" src={logo} />
+      <img className="logo" src={logo} alt="Logo" />
       <nav ref={navRef}>
         <b>
-          <Link to="/home">Home</Link>
+          <Link to="/home" onClick={closeNavbar}>
+            Home
+          </Link>
         </b>
         <b>
-          <Link to="/joblisting">Job Listing</Link>
+          <Link to="/joblisting" onClick={closeNavbar}>
+            Job Listing
+          </Link>
         </b>
         <b>
-          <Link to="/aboutus">About US</Link>
+          <Link to="/aboutus" onClick={closeNavbar}>
+            About US
+          </Link>
         </b>
         <b>
-          <Link to="/blog">Blog</Link>
+          <Link to="/blog" onClick={closeNavbar}>
+            Blog
+          </Link>
         </b>
         <button className="nav-btn nav-close-btn" onClick={showNavbar}>
           <FaTimes />
